60ddf3ce2f1f917aeef05fa96626aa296800347d
Kateryna Shkinova <shkiniova@gmail.com>
2021-10-19 11:18:17 -0400

    modified server.js

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.use(cors());
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/public')))
+  app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '/client/public', 'index.html'))
+  })
+}
+
 app.get('/current', cors(), async (req, res) => {
     try {
         await axios.get('https://xkcd.com/info.0.json').then(response => {
