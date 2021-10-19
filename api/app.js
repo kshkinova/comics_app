6e60cc9959b995bd36ae51a4ce6b3ff92d34ce08
Kateryna Shkinova <shkiniova@gmail.com>
2021-10-19 09:48:42 -0400

    Connected to heroku

--- api/app.js
+++ api/app.js
@@ -2,16 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
-const api_env = `http://localhost:${PORT}`;
 const client_env = "http://localhost:3000"; 
 
 app.use(cors());
-const corsOptions = {
-    origin: client_env
-};
 
 app.get('/current', cors(corsOptions), async (req, res) => {
     try {
@@ -38,5 +34,5 @@ app.get('/:id', cors(corsOptions), async (req, res) => {
 
 
 app.listen(PORT, () => {
-    console.log(`Listening at ${api_env}`);
+    console.log(`Listening at ${PORT}`);
 });
--- client/src/Constants.js
+++ client/src/Constants.js
@@ -1,2 +1,2 @@
 export const CLIENT_ENV = 'http://localhost:3000';
-export const API_ENV = 'http://localhost:5000';
+export const API_ENV = 'https://secret-crag-85774.herokuapp.com';
