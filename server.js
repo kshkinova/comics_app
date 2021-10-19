ffe5afa2fa614ddbdad38b97e5754c3098b22bab
Kateryna Shkinova <shkiniova@gmail.com>
2021-10-19 11:26:47 -0400

    added path middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
